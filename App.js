3bb3b795911630b0e9a2d01ebf453a1507d33a47
SAI <saiteja@SAIs-MacBook-Air.local>
2025-11-03 08:55:58 +0530

    functional component

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
-const heading = React.createElement("div",{id:"parent"},[
-     React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I'am an h1 tag"),
-        React.createElement("h2",{},"I'am an h2 tag"),
-        ]
-    ),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-     React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I'am an h1 tag"),
-        React.createElement("h2",{},"I'am an h2 tag"),
-        ]
-    )
-]
-   
+// const Title = () => (
+// <h1 className="head" tabIndex="5">
+//     Namaste React using JSX
+//     </h1>
+// );
+
+const title = () => (
+<h1 className="head" tabIndex="5">
+    Namaste React using JSX
+    </h1>
 );
 
+
+const HeadingComponent = () => (
+    <div id="container">
+        {title()}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
 
 
 // const heading = React.createElement("h1",
 //     {id:"heading"},
 //     "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
